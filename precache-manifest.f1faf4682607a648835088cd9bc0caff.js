3610080a2cd2cb0b4a63e4dfe65cb85e874f6000
kmoran905 <tyler_moran@hotmail.com>
2023-05-04 23:23:49 -0400

    Updates

---
+++ precache-manifest.f1faf4682607a648835088cd9bc0caff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c142a80f611924adb8a1",
+    "url": "/static/css/main.c96d1ef8.chunk.css"
+  },
+  {
+    "revision": "c142a80f611924adb8a1",
+    "url": "/static/js/main.c142a80f.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "3239a6103dd3d7e59048",
+    "url": "/static/js/2.3239a610.chunk.js"
+  },
+  {
+    "revision": "e01dcd01bb42adeaa836",
+    "url": "/static/js/runtime~main.e01dcd01.js"
+  },
+  {
+    "revision": "1e3d3de7feb941352de5edc7c0b80a53",
+    "url": "/static/media/bluejeans.1e3d3de7.jpg"
+  },
+  {
+    "revision": "6e7b7f1e2e3671d9a4d7d732789a5201",
+    "url": "/static/media/redshirt.6e7b7f1e.jpg"
+  },
+  {
+    "revision": "0aa3a340e014d6bd55cb06318be74fc7",
+    "url": "/static/media/whitesweater.0aa3a340.jpg"
+  },
+  {
+    "revision": "6d1e106795585b754ffc91357652789d",
+    "url": "/index.html"
+  }
+];
